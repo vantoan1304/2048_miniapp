12495d253e47dca64966510b93e850c2ff0dd537
= <=>
2025-12-05 11:16:23 +0700

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,29 @@
 // src/app/layout.tsx
+import type { Metadata } from "next";
 import { Providers } from "./providers";
 import "./globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
+
+export const metadata: Metadata = {
+  title: "Game 2048",
+  description: "Play game 2048",
+  other: {
+    "fc:miniapp": `{
+      "version": "1",
+      "imageUrl": "https://2048-miniapp-three.vercel.app/gm.png",
+      "button": {
+        "title": "Open GMBase",
+        "action": {
+          "type": "launch_frame",
+          "name": "GMBase",
+          "url": "https://2048-miniapp-three.vercel.app/",
+          "splashImageUrl": "https://2048-miniapp-three.vercel.app/gm.png",
+          "splashBackgroundColor": "#000000"
+        }
+      }
+    }`
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
